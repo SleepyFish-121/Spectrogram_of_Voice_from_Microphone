5e23f8e6c5c0594da64e9878d37b45264336ae74
Zongxi <yu_zongxi@outlook.com>
2021-07-15 22:01:18 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import pyaudio
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.mlab as mlab
+from scipy import signal
+from threading import Thread
+import multiprocessing
+import os
+
+RATE = 44100
+CHUNK = 256
+FORMAT = pyaudio.paFloat32
+CHANNELS = 1
+TIME=12
+
+def record(conn_in):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK
+                    )
+    while True:
+        os.write(conn_in.fileno(), stream.read(CHUNK,exception_on_overflow = False))
+
+conn_out, conn_in = multiprocessing.Pipe(duplex=False)
+
+t1 = Thread(target=record, args=(conn_in,))
+t1.start()
+
+fig = plt.figure(figsize=(12, 7))
+ax = fig.add_subplot(1, 1, 1)
+draw_data=np.zeros((1,))
+while True:
+    data=np.frombuffer(os.read(conn_out.fileno(),round(RATE/4)*pyaudio.get_sample_size(FORMAT)), dtype=np.float32)
+    draw_data = np.hstack([draw_data,data])[-RATE*10:]
+    f, t, Sxx = signal.spectrogram(draw_data, mode='magnitude', fs=RATE,nperseg=1024)
+    plt.pcolormesh(t, f, Sxx,shading='auto',norm=colors.LogNorm(vmin=Sxx.min(), vmax=Sxx.max()))
+    print(Sxx)
+
+    plt.draw()
+    plt.pause(0.0001)
+
+    fig.clear()
